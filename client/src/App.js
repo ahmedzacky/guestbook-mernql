78e1fa43da41875de4b70fd9c6ac6a97af939eff
ahmedzacky <ahmazaki97@gmail.com>
2020-09-10 19:39:50 +0200

    frontend (post form created)

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,51 @@
-import React from 'react';
+import React from "react";
+import "bootstrap/dist/css/bootstrap.min.css";
+import { BrowserRouter as Router, Route, Link } from "react-router-dom";
+import Home from "./pages/home";
+import Login from "./pages/login";
+import Signup from "./pages/signup";
 
 function App() {
   return (
-    <div className="App">
-        Hello World
-    </div>
+    <Router>
+      <div className="container">
+        <nav className="navbar navbar-expand-lg navbar-light bg-light">
+          <a
+            className="navbar-brand"
+            href="/"
+			rel="noopener noreferrer"
+          >
+            <img
+              src="https://www.stickerstocker.co.uk/media/catalog/product/cache/1/image/650x/040ec09b1e35df139433887a97daa66f/6/5/65a897_07f1a12b3da84386bbe3329b8989ce43.jpg"
+              width="30"
+              height="30"
+              alt="star"
+            />
+          </a>
+          <Link to="/" className="navbar-brand">
+            Guestbook - challenge
+          </Link>
+          <div className="collpase navbar-collapse">
+            <ul className="navbar-nav mr-auto">
+              <li className="navbar-item">
+                <Link to="/login" className="nav-link">
+                  Login
+                </Link>
+              </li>
+              <li className="navbar-item">
+                <Link to="/signup" className="nav-link">
+                  Signup
+                </Link>
+              </li>
+            </ul>
+          </div>
+        </nav>
+        <br />
+        <Route path="/" exact component={Home} />
+        <Route path="/login" component={Login} />
+        <Route path="/signup" component={Signup} />
+      </div>
+    </Router>
   );
 }
 
---
+++ client/src/pages/home.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import CreateFeedback from '../components/CreateFeedback'
+
+const Home = () => {
+    return (
+        <div>
+            Home
+            <CreateFeedback/>
+        </div>
+    )
+}
+
+export default Home
