e1feb4a487cf1d610c63a1f29f82fe9ba753cea4
ahmedzacky <ahmazaki97@gmail.com>
2020-09-11 19:35:13 +0200

    login mutation -- done

--- server/graphql/resolvers/users.js
+++ server/graphql/resolvers/users.js
@@ -6,33 +6,66 @@ const { secretKey } = require("./../../config");
 
 module.exports = {
 	Mutation: {
+		async login(_parent, { username, password }) {
+			const user = await User.findOne({ username });
+			if (!user) {
+				throw new UserInputError("User not found", {
+					errors: {
+						username: "user not found, try and signup",
+					},
+				});
+			}
+			const match = await bcrypt.compare(password, user.password);
+			if (!match) {
+				throw new UserInputError("Wrong credentials", {
+					errors: {
+						username: "Password doesn't match",
+					},
+				});
+			}
+
+			const token = jwt.sign(
+				{
+					id: user.id,
+					username: user.username,
+				},
+				secretKey,
+				{ expiresIn: "1h" }
+			);
+
+			return {
+				username: user.username,
+				id: user.id,
+				token,
+			};
+		},
 		async register(
 			_parent,
 			{ registerInput: { username, password, confirmPassword } }
 		) {
 			//validate user data
-			if(username.trim() === ''){
+			if (username.trim() === "") {
 				throw new UserInputError("Empty username", {
 					errors: {
 						username: "username must not be empty",
 					},
 				});
 			}
-			if(username.length < 3){
+			if (username.length < 3) {
 				throw new UserInputError("Short Username", {
 					errors: {
 						username: "username must be more than 3 charecters",
 					},
 				});
 			}
-			if(password.length < 3){
+			if (password.length < 3) {
 				throw new UserInputError("Short password", {
 					errors: {
 						password: "password must be more than 3 charecters",
 					},
 				});
 			}
-			if(password !== confirmPassword){
+			if (password !== confirmPassword) {
 				throw new UserInputError("unmatching passwords", {
 					errors: {
 						password: "passwords must match",
@@ -49,7 +82,7 @@ module.exports = {
 					},
 				});
 			}
-			
+
 			//hash the password and return auth token
 			password = await bcrypt.hash(password, 12);
 			const newUser = new User({
--- server/graphql/typeDefs.js
+++ server/graphql/typeDefs.js
@@ -23,5 +23,6 @@ module.exports = gql`
   }
   type Mutation{
     register(registerInput: RegisterInput): User!
+    login(username: String!, password: String!): User!
   }
 `;
