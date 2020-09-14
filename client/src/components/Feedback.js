5dc6e251aeec9b1d9b755bcbfe7ff5928b51bde7
ahmedzacky <ahmazaki97@gmail.com>
2020-09-14 18:32:11 +0200

    added feedback gadgets to homepage

--- client/src/components/Feedback.js
+++ client/src/components/Feedback.js
@@ -1,29 +1,34 @@
-import React from "react";
+import React, { useContext } from "react";
 import moment from "moment";
 import { Link } from "react-router-dom";
+import { AuthContext } from "./../context/auth";
+import EditGadget from "./EditGadget";
+import DeleteGadget from "./DeleteGadget";
 
-export const Feedback = ({
-	body,
-	replies,
-	updatedAt,
-	username,
-	id,
-}) => {
+export const Feedback = ({ body, replies, updatedAt, username, id }) => {
+	const { user } = useContext(AuthContext);
 	return (
-		<div className="jumbotron pt-4 pb-2" >
+		<div className="jumbotron pt-4 pb-2">
 			<em className="d-inline">Posted by: {username}</em>
 			<em className="d-inline float-right">
-				<Link to={`feedbacks/${id}`}>
-					Updated {moment(updatedAt).fromNow()}
+				<Link to={`/feedbacks/${id}`}>
+					Last Updated:{" "}
+					{moment(updatedAt).fromNow()}
 				</Link>
+				{user && user.username === username && (
+					<>
+						<EditGadget body={body} id={id} />
+						<DeleteGadget id={id} />
+					</>
+				)}
 			</em>
 			<p className="lead my-4">{body}</p>
 			{replies.length > 0 ? (
-				<Link to={`feedbacks/${id}`}>
+				<Link to={`/feedbacks/${id}`}>
 					<p>Replies: {replies.length}</p>
 				</Link>
 			) : (
-				<Link to={`feedbacks/${id}`}>
+				<Link to={`/feedbacks/${id}`}>
 					<p>Reply to Feedback</p>
 				</Link>
 			)}
